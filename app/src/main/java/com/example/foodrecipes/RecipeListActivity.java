52daf8f7f0d48fa2b08d1cf4036131b3246a7b9a
akhil-coder <akhil.honor@gmail.com>
2020-06-13 10:25:30 +0530

    RecyclerView setup completed

--- app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/example/foodrecipes/RecipeListActivity.java
@@ -1,47 +1,35 @@
 package com.example.foodrecipes;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodrecipes.adapters.OnRecipeListener;
+import com.example.foodrecipes.adapters.RecipeRecyclerAdapter;
 import com.example.foodrecipes.models.Recipe;
-import com.example.foodrecipes.requests.RecipeApi;
-import com.example.foodrecipes.requests.ServiceGenerator;
-import com.example.foodrecipes.requests.response.RecipeSearchResponse;
-import com.example.foodrecipes.util.Constants;
 import com.example.foodrecipes.util.Testing;
 import com.example.foodrecipes.viewmodels.RecipeListViewModel;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class RecipeListActivity extends BaseActivity {
+public class RecipeListActivity extends BaseActivity implements OnRecipeListener {
     private static final String TAG = "RecipeListActivity";
     private RecipeListViewModel mRecipeListViewModel;
-
+    private RecyclerView mRecylerView;
+    private RecipeRecyclerAdapter mRecipeRecyclerAdapter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+        mRecylerView = findViewById(R.id.recipe_list);
         mRecipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
+        initRecyclerView();
         subscribeObservers();
-
-        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                testRetrofitRequest();
-            }
-        });
+        testRetrofitRequest();
     }
 
     private void subscribeObservers(){
@@ -50,11 +38,16 @@ public class RecipeListActivity extends BaseActivity {
             public void onChanged(List<Recipe> recipes) {
                 if(recipes != null){
                     Testing.printRecipes(recipes);
+                    mRecipeRecyclerAdapter.setmRecipes(recipes);
             }
             }
         });
     }
-
+    private void initRecyclerView(){
+        mRecipeRecyclerAdapter = new RecipeRecyclerAdapter( this);
+        mRecylerView.setAdapter(mRecipeRecyclerAdapter);
+        mRecylerView.setLayoutManager(new LinearLayoutManager(this));
+    }
     public void searchRecipeApi(String query, int pageNumber)
     {
         mRecipeListViewModel.searchRecipeApi(query, pageNumber);
@@ -62,5 +55,15 @@ public class RecipeListActivity extends BaseActivity {
 
     private void testRetrofitRequest(){
        searchRecipeApi("Chicken breast", 1);
+    }
+
+    @Override
+    public void onRecipeClick(int position) {
+
+    }
+
+    @Override
+    public void onCategoryClick(String category) {
+
     }
 }
